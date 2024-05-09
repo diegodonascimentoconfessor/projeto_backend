0d16432e049adf51b5cf6faa8228e8af5c4afb2a
diegodonascimentoconfessor <153656081+diegodonascimentoconfessor@users.noreply.github.com>
2024-05-09 16:13:07 -0300

    atividade_revisão_js

--- exercicio.js
+++ exercicio.js
@@ -69,3 +69,32 @@
 
 
 // exercicio 4
+
+// ## Soma dos pares
+
+// Declare um array com alguns números inteiros quaisquer.
+
+// Depois, percorra este array e calcule a **soma dos números pares** presentes nesse array.
+
+// Exemplo:
+
+// Para o array **numeros** abaixo
+
+// ```javascript
+// const numeros = [3, 4, 7, 8, 1, 6, 5, 10];
+
+// // seu codigo aqui
+// ```
+
+// Deverá ser impresso no console:
+
+const numeros = [3, 4, 7, 8, 1, 6, 5, 10];
+let soma = 0;
+
+for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] % 2 == 0) {
+        soma += numeros[i];
+    }
+}
+
+console.log(soma);
